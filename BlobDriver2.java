afa08c536fbb1ff2e18b62d13084ef7d1ab199be
Yuri Campos <62523486+Yuri-Campos@users.noreply.github.com>
2022-05-26 20:35:47 -0300

    Rename blobDriver2.java to BlobDriver2.java

--- BlobDriver2.java
+++ BlobDriver2.java
@@ -1,7 +1,7 @@
 
 public class BlobDriver2 {
 
-	//declarando vari·veis do tipo Blob
+	//declarando vari√°veis do tipo Blob
 		private Blob0 blob1;
 		private Blob0 blob2;
 		
@@ -12,18 +12,18 @@ public class BlobDriver2 {
 			this.blob2 = blob2;
 		}
 		
-		//mÈtodo de teste
+		//m√©todo de teste
 		public void test1()
 		{
 			//Define o raio do blob1 baseado no raio do blob2;
 			blob1.setR(2 * blob2.getR());
-			//Define a velocidade do blob2, os parametros passados s„o armazenados nas vari·veis dx e dy;
+			//Define a velocidade do blob2, os parametros passados s√£o armazenados nas vari√°veis dx e dy;
 			blob2.setVelocity(3, 4);
-			//atualiza o raio e a posiÁ„o do blob baseado no crescimento e na velocidade do mesmo.
+			//atualiza o raio e a posi√ß√£o do blob baseado no crescimento e na velocidade do mesmo.
 			blob2.step();
-			//define o crescimento do blob o parametro È armazenado na variavel dr
+			//define o crescimento do blob o parametro √© armazenado na variavel dr
 			blob1.setGrowth(10);
-			//atualiza o raio e a posiÁ„o do blob baseado no crescimento e na velocidade do mesmo.
+			//atualiza o raio e a posi√ß√£o do blob baseado no crescimento e na velocidade do mesmo.
 			blob1.step();
 			
 			System.out.println("Blob 1: at" + blob1.getX() + " ," + "; sized" + blob2.getR());
